d5247261abb9d5ddee46b13de9afcae481931549
piousck <pious.james007@gmail.com>
2021-12-12 13:03:02 +0000

    commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+          child: ElevatedButton(
+        onPressed: () {},
+        child: Text("Press Me"),
+      )),
+    );
+  }
+}
---
+++ lib/notification_service.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class MyNotifyScreen extends StatefulWidget {
+  MyNotifyScreen({Key? key}) : super(key: key);
+
+  @override
+  _MyNotifyScreenState createState() => _MyNotifyScreenState();
+}
+
+class _MyNotifyScreenState extends State<MyNotifyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
